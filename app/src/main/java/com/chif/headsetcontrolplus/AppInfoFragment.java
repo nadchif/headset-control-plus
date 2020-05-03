73afdc4c5f406ae317568551988ae9797ea944df
nadchif <nadchif@gmail.com>
2020-05-03 15:49:31 +0200

    optimization tweaks

--- app/src/main/java/com/chif/headsetcontrolplus/AppInfoFragment.java
+++ app/src/main/java/com/chif/headsetcontrolplus/AppInfoFragment.java
@@ -1,12 +1,17 @@
+/**
+ * This fragment provides the App Information/About screen.
+ */
+
 package com.chif.headsetcontrolplus;
 
 import android.os.Bundle;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class AppInfoFragment extends PreferenceFragmentCompat {
-  public AppInfoFragment(){
+  //required empty constructor to prevent app crash
+  public AppInfoFragment() {
+  }
 
-  };
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     setPreferencesFromResource(R.xml.appinfo_preferences, rootKey);
